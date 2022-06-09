98d5f381a10d6c2dbae2c581e776875b33467679
pankaj <pankajthekush@gmail.com>
2022-06-09 13:14:05 +0530

    updated setup tools

--- setup.py
+++ setup.py
@@ -1,16 +1,2 @@
-from distutils.core import setup
-from setuptools import Extension,find_packages
-from os import path
-
-setup(
-    name = 'pnmatcher',
-    version = '0.1.2',
-    description = 'match and extract phone numbers in url and text',
-    author = 'Lingzhe Teng',
-    author_email = 'zwein27@gmail.com',
-    url = 'https://github.com/ZwEin27/phone-number-matcher',
-    download_url = 'https://github.com/ZwEin27/phone-number-matcher/tarball/0.1.2',
-    packages = find_packages(),
-    keywords = ['phone_number', 'extractor'],
-    install_requires=['phonenumbers', 'digSparkUtil']
-)
+from setuptools import setup
+setup()
