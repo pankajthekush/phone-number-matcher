cd162b85c9fe17c10b767a219343791285248242
pankaj <pankajthekush@gmail.com>
2022-06-09 12:40:43 +0530

    removed redundent files

--- example.py
+++ example.py
@@ -1,13 +1,8 @@
 from pnmatcher import PhoneNumberMatcher
 matcher = PhoneNumberMatcher()
-import json
-#url_string = 'http://www.paducahwomensclinic.com/Obstetrics/Obstetrics.html'
-#url_phone_numbers = matcher.match(url_string, source_type='url')
-# []
-
-#with open('/home/pankaj/Downloads/22994248.json','r') as f:
-#    jdata = json.load(f)
-#    vtext = jdata['url_visible_text']
+url_string = 'http://www.paducahwomensclinic.com/Obstetrics/Obstetrics.html1909898878'
+url_phone_numbers = matcher.match(url_string, source_type='url')
+print(url_phone_numbers)
 
 text_string = 'this is my number 1 seven zero 1 one eight five five two one'
 text_phone_numbers = matcher.match(text_string, source_type='text')
